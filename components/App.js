b9d5caa4eebd10af4e8717e41d897164a3140be7
Kyhan Turner <kyhanpt@gmail.com>
2016-11-07 15:34:20 -0800

    setup socket.io server & client

--- components/App.js
+++ components/App.js
@@ -1,6 +1,17 @@
 var React = require('react');
+var io = require('socket.io-client');
+
 
 var App = React.createClass({
+	componentWillMount() {
+		this.socket = io('http://localhost:3000');
+		this.socket.on('connect', this.connect);
+	},
+
+	connect() {
+		alert("Connected: " + this.socket.id);
+	}, 
+
 	render() {
 		return (<h1>Hello World form React</h1>);
 	}
